c576be235eb8cf4e45c0e0472a962b56739ee80b
Jonathan-Majors <izzymajors@gmail.com>
2021-01-25 18:16:08 -0600

    imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Route, Link, Switch} from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
 
@@ -29,6 +30,7 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
+      <Link to='./Movies/SavedList'>SavedList</Link>
 
       <div>Replace this Div with your Routes</div>
     </div>
